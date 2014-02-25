3a13058b6a2e260f3ee31b7e7e6e538220b9bab2
Lorenxs <lorenxs@gmail.com>
2014-02-26 00:22:22 +0100

    Implementato network prediction e interpolation per rendere piu' fluidi i movimenti dei giocatori remoti.

--- Assets/Scripts/NetworkManager.cs
+++ Assets/Scripts/NetworkManager.cs
@@ -132,5 +132,5 @@ public class NetworkManager : MonoBehaviour {
 			playerNumber =players;
 			
 	}
-
+	
 }
--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -7,6 +7,14 @@ public class PlayerMovement : MonoBehaviour {
 	public float gravity = 20.0F;
 	private Vector3 moveDirection = Vector3.zero;
 	PlayerManager PMC_PlayerManagerClass;
+
+	private float lastSynchronizationTime = 0f;
+	private float syncDelay = 0f;
+	private float syncTime = 0f;
+	private Vector3 startSyncPosition;
+	private Vector3 endSyncPosition;
+	private Quaternion startSyncRotation;
+	private Quaternion endSyncRotation;
 	
 	// Use this for initialization
 	void Start () {
@@ -20,25 +28,61 @@ public class PlayerMovement : MonoBehaviour {
 
 		rigidbody.WakeUp();
 		//movement and lookAt
-		if(networkView.isMine){
-			CharacterController controller = GetComponent<CharacterController>();
-			moveDirection = new Vector3(Input.GetAxis("Horizontal"),moveDirection.y, Input.GetAxis("Vertical"));
+		if (networkView.isMine) {
+			CharacterController controller = GetComponent<CharacterController> ();
+			moveDirection = new Vector3 (Input.GetAxis ("Horizontal"), moveDirection.y, Input.GetAxis ("Vertical"));
 			moveDirection.x *= speed;
 			moveDirection.z *= speed;
 			if (controller.isGrounded) {
-				moveDirection.y = 0;
-				if (Input.GetButton("Jump")){
-					moveDirection.y = jumpSpeed;
-					PMC_PlayerManagerClass.PlaySound("jump");
-				}
+					moveDirection.y = 0;
+					if (Input.GetButton ("Jump")) {
+							moveDirection.y = jumpSpeed;
+							PMC_PlayerManagerClass.PlaySound ("jump");
+					}
 
 			}
 			moveDirection.y -= gravity * Time.deltaTime;
-			controller.Move(moveDirection * Time.deltaTime);
-			Vector3 faceDirection = new Vector3(moveDirection.x,0,moveDirection.z);
-			if(faceDirection!= Vector3.zero)
-				transform.forward = Vector3.Normalize(faceDirection);
-				PMC_PlayerManagerClass.lookDirection = faceDirection;
+			controller.Move (moveDirection * Time.deltaTime);
+			Vector3 faceDirection = new Vector3 (moveDirection.x, 0, moveDirection.z);
+			if (faceDirection != Vector3.zero)
+					transform.forward = Vector3.Normalize (faceDirection);
+			PMC_PlayerManagerClass.lookDirection = faceDirection;
+		} else {
+			syncTime += Time.deltaTime;
+			rigidbody.position = Vector3.Lerp(startSyncPosition, endSyncPosition, syncTime / syncDelay);
+			rigidbody.rotation = Quaternion.Lerp(startSyncRotation, endSyncRotation, syncTime / syncDelay);
+		}
+	}
+
+	void OnSerializeNetworkView(BitStream stream, NetworkMessageInfo info)
+	{
+		Vector3 syncPosition = Vector3.zero;
+		Quaternion syncRotation = Quaternion.identity;
+		Vector3 syncVelocity = Vector3.zero;
+		if (stream.isWriting)
+		{
+			syncPosition = rigidbody.position;
+			syncRotation = rigidbody.rotation;
+			syncVelocity = rigidbody.velocity;
+			stream.Serialize(ref syncPosition);
+			stream.Serialize(ref syncRotation);
+			stream.Serialize(ref syncVelocity);
+		}
+		else
+		{
+			stream.Serialize(ref syncPosition);
+			stream.Serialize(ref syncRotation);
+			stream.Serialize(ref syncVelocity);
+			
+			syncTime = 0f;
+			float currentTime = Time.time;
+			syncDelay = currentTime - lastSynchronizationTime;
+			lastSynchronizationTime = currentTime;
+			
+			startSyncPosition = rigidbody.position;
+			startSyncRotation = rigidbody.rotation;
+			endSyncPosition = syncPosition + syncVelocity * syncDelay;
+			endSyncRotation = syncRotation;
 		}
 	}
 }
